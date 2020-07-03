66af623fb788a41d049c856facda5f920a5b3705
gmart <martinetti.gregoire@gmail.com>
2020-07-03 17:43:08 +0200

    add ngroc enable, and live reload stuf

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ const config = {
     contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 1234,
-    disableHostCheck: true
+    disableHostCheck: true, //=> to enable ngrok
+
+    open: true, //open page when server start
+    inline: true, //webpack can log in console
+    //for liveReload:
+    watchContentBase: true,
+    liveReload: true,
+    //////////////
   }
 };
 
